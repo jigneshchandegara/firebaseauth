b0c0a6d650c7fb0306b864fd42d72d13e4bcb30b
jigneshchandegara <chandegarajignesh29@gmail.com>
2024-05-21 15:15:35 +0530

    firebaseauth

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-mgy6mr0z6a3urard.us.auth0.com"
+    clientId="9FLycoCeVzNRksOjJ9Bm0sN6yjC5OnQo"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
